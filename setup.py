1c1f3a42d9c26e4c5a70f83f535b0c6606021c8e
seangao <seancty@gmail.com>
2019-02-11 16:29:17 -0500

    package setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/ python
+
+from distutils.core import setup
+
+setup(name='Smith-Waterman',
+      version='1.0',
+      description=' Smith-Waterman local alignment algorithm for protein sequences',
+      author='Sean Gao',
+      author_email='sean.gao@yale.edu',
+      packages=['smith_waterman'],
+     )
