1053b10bf6ed7d6a5fd6797b129382867c55be89
seangao <seancty@gmail.com>
2019-02-18 15:57:13 -0500

    sw slightly off

--- smith_waterman/sw.py
+++ smith_waterman/sw.py
@@ -34,7 +34,7 @@ def runSW(inputFile, scoreFile, openGap, extGap):
 
     scoreRows = []
     scoreCols = []
-    scoreMatrix = []
+    S = []
     with open(scoreFile) as f:
         lines = f.readlines()
         scoreCols = lines[0].strip().split()
@@ -43,8 +43,52 @@ def runSW(inputFile, scoreFile, openGap, extGap):
             if line:
                 scoreRows.append(line[0])
                 row = [float(i) for i in line[1:]]
-                scoreMatrix.append(row)
-    scoreMatrix = np.matrix(scoreMatrix)
+                S.append(row)
+    SRows = {k: v for v, k in enumerate(scoreRows)}
+    SCols = {k: v for v, k in enumerate(scoreCols)}
+    S = np.matrix(S)
+
+    a = seqs[0]
+    b = seqs[1]
+    n = len(a)
+    m = len(b)
+    H = np.zeros((n + 1, m + 1))
+
+    W = np.arange(openGap, openGap + extGap * max(n, m), extGap)
+
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            H[i][j] = max(H[i - 1][j - 1] + S[SRows[a[i - 1]], SCols[b[j - 1]]],
+                max([H[i - k][j] + W[k - 1] for k in range(1, i + 1)]),
+                max([H[i][j - l] + W[l - 1] for l in range(1, j + 1)]),
+                0)
+
+    ind = np.unravel_index(np.argmax(H, axis=None), H.shape)
+    np.set_printoptions(threshold=np.nan)
+    print H
+    print H[ind]
+    anew = a[ind[0] - 1]
+    bnew = b[ind[1] - 1]
+    while H[ind] != 0:
+        next = max(H[ind[0] - 1, ind[1] - 1],
+            H[ind[0] - 1, ind[1]],
+            H[ind[0], ind[1] - 1])
+        if next == H[ind[0] - 1, ind[1] - 1]:
+            ind = ind[0] - 1, ind[1] - 1
+            anew += a[ind[0] - 1]
+            bnew += b[ind[1] - 1]
+        elif next == H[ind[0] - 1, ind[1]]:
+            ind = ind[0] - 1, ind[1]
+            anew += a[ind[0] - 1]
+            bnew = bnew[:-1] + '-' + bnew[-1]
+        else:
+            ind = ind[0], ind[1] - 1
+            anew = anew[:-1] + '-' + anew[-1]
+            bnew += b[ind[1] - 1]
+
+    print anew[::-1]
+    print bnew[::-1]
+
 
 
 ### Print input and score file names.
